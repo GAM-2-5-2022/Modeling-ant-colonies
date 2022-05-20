3bc1472df978630b8e3df63edea4f37b8474a6a0
RokoBrl <105510984+RokoBrl@users.noreply.github.com>
2022-05-20 14:26:23 +0200

    Added python script that contains the code for the actual simulation
    Added python script that contains the code for the actual simulation. This simulation is performed purely mathematically using differential equations.

---
+++ antSiumlation.py
@@ -0,0 +1,39 @@
+import numpy as np
+args = {"a":10, "b":1} #[food_coefficient, colony_coefficient]
+state_0 = [2, 2] #[food, colony]
+
+def food_d(colony, a):return -a*colony #the food will decrease more the bigger the colony is. (added coefficient a)
+def colony_d(food, b):return b*food #the colony will increase more the bigger the food supply is. (added coefficient b)
+d_state = [food_d, colony_d]
+
+
+def state(t, dt, state_0, d_state, args):
+	state_n = state_0
+	x_range = [state_0[0]]
+	y_range = [state_0[1]]
+	print(args)
+	for n in np.arange(0, t, dt):
+		#this finds the derivatives at the points described in the state_n array
+		d_x, d_y = [d_state[0](state_n[1], args["a"]), d_state[1](state_n[0], args["b"])] 
+
+		# only uncomment this if you dont care about your terminal
+		# print(state_n)
+
+		#changes the states and adds them to a arrays for plotting in matplotlib
+		state_n[0] += d_x*dt 
+		state_n[1] += d_y*dt
+		x_range.append(state_n[0])
+		y_range.append(state_n[1])
+	
+	return [x_range, y_range]
+
+food, colony = state(10, 0.01, state_0, d_state, args)
+print("max COL:" + str(max(colony)))
+print("max FOOD:" + str(max(food)))
+
+def writeDataToFile(arr1,label1, arr2, label2):
+	f = open("./results.txt", "w")
+	for i in range(0, len(arr1)):
+		f.write(f"{label1}: {arr1[i]}, {label2}: {arr2[i]} \n")
+
+writeDataToFile(food,"FOOD",colony,"COLONY")
